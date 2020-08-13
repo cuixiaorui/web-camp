0706cd50d2141daa9658621c14e9f1c543d5c4fc
cuixiaorui <cui_xiaorui@126.com>
2020-08-13 14:40:26 +0800

    feat(play-plane): add hitTestObject

---
+++ vue/play-plane/src/utils/index.js
@@ -0,0 +1,8 @@
+export function hitTestObject(objA, objB) {
+  return (
+    objA.x + objA.width >= objB.x &&
+    objB.x + objB.width >= objA.x &&
+    objA.y + objA.height >= objB.y &&
+    objB.y + objB.height >= objA.y
+  );
+}

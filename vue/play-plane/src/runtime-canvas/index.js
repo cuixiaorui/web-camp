b9403aebcd69852ce8badfea3fcba358503abc2b
cuixiaorui <cui_xiaorui@126.com>
2020-08-13 12:50:33 +0800

    feat(play-plane): add renderer interface logic

--- vue/play-plane/src/runtime-canvas/index.js
+++ vue/play-plane/src/runtime-canvas/index.js
@@ -46,6 +46,20 @@ const renderer = createRenderer({
     parent.addChild(el);
     console.log(el, parent);
   },
+  // 新加接口实现
+  // 处理注释
+  createComment() {},
+  // 获取父节点
+  parentNode() {},
+  // 获取兄弟节点
+  nextSibling() {},
+  // 删除节点时调用
+  remove(el) {
+    const parent = el.parent;
+    if (parent) {
+      parent.removeChild(el);
+    }
+  },
 });
 
 export function createApp(rootComponent) {
